93f855dd6b066f9b056bab2592950dfd276397be
jackstivers1 <jstiver@clemson.edu>
2026-02-08 19:59:56 -0500

    Update get_items.py

--- netlify/functions/get_items.py
+++ netlify/functions/get_items.py
@@ -1,16 +1,14 @@
-import json
-import os
+import json, os
 
 def handler(event, context):
     path = os.path.join(os.path.dirname(__file__), "data.json")
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            json.dump([], f)
     with open(path, "r") as f:
         data = json.load(f)
-
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*"
-        },
+        "headers": {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
         "body": json.dumps(data)
     }
